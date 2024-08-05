0c364db4be76d1341edca12d6bddbdaccf9b68a4
RegiOli <167256752+RegiOli@users.noreply.github.com>
2024-08-05 20:46:32 -0300

    Update admin.py
    para poder ver desde admin las respuestas al inicio

--- pagmata/core/admin.py
+++ pagmata/core/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import RespuestaFormulario, RespuestaDestinoFinal
+from .models import Respuesta
 
 admin.site.register(RespuestaFormulario)
 admin.site.register(RespuestaDestinoFinal)
+
+class RespuestaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'apellido', 'jugar', 'caminar', 'num1')
+
+admin.site.register(Respuesta, RespuestaAdmin)
